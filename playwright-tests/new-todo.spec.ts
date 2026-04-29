91b42cf9c73dd077f64da05376dc33f2f34eb466
miguelangarano <miguelangarano@gmail.com>
2026-04-29 00:39:59 -0500

    testing solution

---
+++ playwright-tests/new-todo.spec.ts
@@ -0,0 +1,11 @@
+import { expect, test } from '@playwright/test';
+
+test('creates a new todo', async ({ page }) => {
+  await page.goto('/');
+  await expect(page.getByRole('heading', { name: 'todos' })).toBeVisible();
+
+  await page.getByPlaceholder('What needs to be done?').fill('Demo');
+  await page.getByPlaceholder('What needs to be done?').press('Enter');
+
+  await expect(page.locator('.todo-list')).toContainText('Demo');
+});
